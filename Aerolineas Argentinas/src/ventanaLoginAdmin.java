0cd773339590434e5eb68b299e2d12c2aa60d0ba
BatraChin <guillef92@gmail.com>
2019-10-09 23:58:33 -0300

    asd

--- Aerolineas Argentinas/src/ventanaLoginAdmin.java
+++ Aerolineas Argentinas/src/ventanaLoginAdmin.java
@@ -34,6 +34,7 @@ public class ventanaLoginAdmin extends JFrame {
 	 * Create the frame.
 	 */
 	public ventanaLoginAdmin() {
+		dbVuelos = new VuelosConexion();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
