9bb2cfa8e38a0f0dde099b071a8510367fcb1e3b
BatraChin <guillef92@gmail.com>
2019-11-30 16:52:34 -0300

    quedo

--- Aerolineas Argentinas/src/ReservasGUI.java
+++ Aerolineas Argentinas/src/ReservasGUI.java
@@ -573,11 +573,9 @@ public class ReservasGUI {
 			        	String llegada=(String) tableVuelosIda.getValueAt(row,4);
 			        	Time h_llegada= (Time) tableVuelosIda.getValueAt(row,5);
 			        	String modelo=(String) tableVuelosIda.getValueAt(row,6);
-<<<<<<< HEAD
+			        	
 			        	String query= "SELECT DISTINCT Clase, Asientos_disponibles,Precio FROM vuelos_disponibles WHERE Cod_vuelo='"+numero+"' AND Fecha='"+fechaS+"' AND Aeropuerto_origen='"+salida+"' AND Hora_salida='"+h_salida+"' AND Aeropuerto_destino='"+llegada+"' AND Hora_llegada='"+h_llegada+"' AND Modelo_avion='"+modelo+"';";
-=======
-			        	String query="SELECT DISTINCT Clase, Asientos_disponibles,Precio FROM vuelos_disponibles WHERE Cod_vuelo='"+numero+"' AND Fecha='"+fechaS+"' AND Aeropuerto_origen='"+salida+"' AND Hora_salida='"+h_salida+"' AND Aeropuerto_destino='"+llegada+"' AND Hora_llegada='"+h_llegada+"' AND Modelo_avion='"+modelo+"';" ;
->>>>>>> parent of 836b2e6... Revert "Update ReservasGUI.java"
+
 			        	//Muestra los datos de un determinado vuelo
 			        	m.refrescarTabla(query,tableClasesIda,v);
 						tableClasesIda.setVisible(true);
@@ -620,7 +618,7 @@ public class ReservasGUI {
 					int row = tableVuelosVuelta.rowAtPoint(arg0.getPoint());
 			        if (row >= 0) {
 			        	String numero=(String) tableVuelosVuelta.getValueAt(row,0);
-			        	Date fechaV= (Date) tableVuelosIda.getValueAt(row,1);
+			        	Date fechaV= (Date) tableVuelosVuelta.getValueAt(row,1);
 			        	String salida= (String) tableVuelosVuelta.getValueAt(row,2);
 			        	Time h_salida= (Time) tableVuelosVuelta.getValueAt(row,3);
 			        	String llegada=(String) tableVuelosVuelta.getValueAt(row,4);
